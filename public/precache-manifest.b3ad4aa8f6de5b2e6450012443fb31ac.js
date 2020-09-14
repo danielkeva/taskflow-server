fc3ae010b7a3791631a5e429d8fff0d0dc6fce4c
Daniel Keva <danielkeva@Daniels-MacBook-Pro.local>
2020-09-14 12:08:07 +0300

    add static.json

---
+++ public/precache-manifest.b3ad4aa8f6de5b2e6450012443fb31ac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d08431d821b441abbd21cbb798b8992f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "896e91b2d7734839d7fd",
+    "url": "/static/css/2.a12547f9.chunk.css"
+  },
+  {
+    "revision": "d8a036ea8af9e2cca6f6",
+    "url": "/static/css/main.54e8d70f.chunk.css"
+  },
+  {
+    "revision": "896e91b2d7734839d7fd",
+    "url": "/static/js/2.3f0488f4.chunk.js"
+  },
+  {
+    "revision": "ac1dd574077a842e17e236a143e07477",
+    "url": "/static/js/2.3f0488f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8a036ea8af9e2cca6f6",
+    "url": "/static/js/main.ce0da25e.chunk.js"
+  },
+  {
+    "revision": "c16fb3296af5d12b2d51",
+    "url": "/static/js/runtime-main.d905624a.js"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/static/media/Lato-Regular.2d36b1a9.ttf"
+  }
+]);
