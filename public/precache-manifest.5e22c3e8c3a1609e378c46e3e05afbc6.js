40226549b13736f69710984198140cba5b466f3b
Daniel Keva <danielkeva@Daniels-MacBook-Pro.local>
2020-09-14 13:59:44 +0300

    fix routing

---
+++ public/precache-manifest.5e22c3e8c3a1609e378c46e3e05afbc6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d40386a0ba6eb65ace2cf26b827c7391",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be3916d1e6d90d5713c8",
+    "url": "/static/css/2.a12547f9.chunk.css"
+  },
+  {
+    "revision": "2b443c8d741c7af7a7e6",
+    "url": "/static/css/main.54e8d70f.chunk.css"
+  },
+  {
+    "revision": "be3916d1e6d90d5713c8",
+    "url": "/static/js/2.c5930531.chunk.js"
+  },
+  {
+    "revision": "ac1dd574077a842e17e236a143e07477",
+    "url": "/static/js/2.c5930531.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b443c8d741c7af7a7e6",
+    "url": "/static/js/main.cec55b37.chunk.js"
+  },
+  {
+    "revision": "c16fb3296af5d12b2d51",
+    "url": "/static/js/runtime-main.d905624a.js"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/static/media/Lato-Regular.2d36b1a9.ttf"
+  }
+]);
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.10838ecf7571851eec662b0e92576d52.js"
+  "/precache-manifest.5e22c3e8c3a1609e378c46e3e05afbc6.js"
 );
 
 self.addEventListener('message', (event) => {
