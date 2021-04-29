c10a64457888121ee660e34adcce61ef61c86338
Daniel Keva <danielk@webiya.co.il>
2021-04-29 14:17:22 +0300

    added requireBoardAuth middleware

--- api/board/board.routes.js
+++ api/board/board.routes.js
@@ -1,14 +1,14 @@
-const express = require('express')
-const { requireAuth, requireAdmin, requireBoardAuth } = require('../../middlewares/requireAuth.middleware')
-const { getBoards, getBoard, updateBoard,addBoard } = require('./board.controller')
-const router = express.Router()
+const express = require('express');
+const { requireAuth, requireAdmin, requireBoardAuth } = require('../../middlewares/requireAuth.middleware');
+const { getBoards, getBoard, updateBoard, addBoard } = require('./board.controller');
+const router = express.Router();
 
-router.get('/',requireAuth, getBoards)
-router.get('/:id', getBoard)
+router.get('/', requireBoardAuth, getBoards);
+router.get('/:id', getBoard);
 // router.get('/:id',requireAuth, getBoard)
-router.post('/',requireAuth, addBoard)
-router.put('/:id', updateBoard)
+router.post('/', requireAuth, addBoard);
+router.put('/:id', updateBoard);
 // router.post('/',  requireAuth, addReview)
 // router.delete('/:id',  requireAuth, deleteReview)
 
-module.exports = router
+module.exports = router;
